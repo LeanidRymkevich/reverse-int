b9e1ca1acc817cd4e78ff27217d464fbb5481e70
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-01-04 12:43:01 +0300

    feat: adding the task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function reverse (n) {
-  
+
+  let numStr = n.toString();
+  let result = '';
+
+  if(n < 0){
+    numStr = numStr.slice(1, numStr.length);
+  }
+
+  for(let i = numStr.length - 1; i >= 0; i--){
+    result += numStr[i];
+  }
+
+  if(result[0] === 0 && result.length !== 0){
+    result = result.slice(1, result.length);
+  }
+
+  return parseInt(result);
 }
+
